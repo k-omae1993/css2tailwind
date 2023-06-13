d880227742f53ab9c8c408b5c78b35a1c6545a26
Koichi Omae <ohmae@arubeh.com>
2023-06-13 22:03:01 +0900

    Vercelのアナリティクス設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
+      <Analytics />
     </RecoilRoot>
   );
 }
